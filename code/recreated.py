0e8a13187380647954d4c54fb53b328d4334a5b0
nrivkin <noah.rivkin@students.olin.edu>
2017-09-26 10:26:54 -0400

    Added outline for the recreated experiment

---
+++ code/recreated.py
@@ -0,0 +1,32 @@
+""" a crecreation of the spatial network thing
+Noah Rivkin and 
+"""
+
+import networkx
+import numpy as np
+
+class Node():
+    def __init__(state):
+        """
+        node object in graph. state is C or D
+        """
+        # TODO: make function
+        self.state = state
+        pass
+
+    def update(self):
+        """
+        timestep for node
+        """
+        # TODO: timestep
+        pass
+
+# TODO: create graph
+
+# TODO: create rules
+
+# TODO: create node object
+
+# TODO: create main
+
+# TODO: create test suite
