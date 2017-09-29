439328b2a0e3bc44568092a0249cfffba1d76788
changjun lim <ailuropoda0@gmail.com>
2017-09-29 17:22:00 -0400

    fixed error

--- code/Spatialnet.py
+++ code/Spatialnet.py
@@ -1,9 +1,10 @@
 import networkx
 import numpy as np
 
+
 class Network():
-    def __init__(n, k, type='smallworld'):
-        G = create_graph(n, k, type)
+    def __init__(self, n, k, type='smallworld'):
+        G = self.create_graph(n, k, type)
         self.G = G
 
     def create_graph(self, n, k, type):
