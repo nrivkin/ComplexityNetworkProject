3456725109c2267f84fa91222ed5bfbb13b59a49
changjun lim <ailuropoda0@gmail.com>
2017-10-07 12:18:22 -0400

    update to testbench file

--- code/PDnode.py
+++ code/PDnode.py
@@ -1,26 +1,22 @@
-import random
-
-
 class Node:
     C, D = 'C', 'D'
-    T, R, P, S = 1, 0.25, 0, 0
+    T, R, P, S = 3, 1, 0, 0
 
-    def __init__(self, state):
+    def __init__(self, state: str):
         """
         node object in graph. state is C or D
         """
         self.state = state
-        self.max_state = ''
         self.score = 0  # will hold value calculated based on prisoners dilemma
 
     @classmethod
-    def is_cooperator_state(cls, state):
+    def is_cooperator_state(cls, state: str):
         return state == cls.C
 
     def is_cooperator(self):
         return self.is_cooperator_state(self.state)
 
-    def add_score(self, score):
+    def add_score(self, score: int):
         self.score += score
 
     def reset_score(self):
@@ -29,15 +25,12 @@ class Node:
     # Play a single PD with neighbors, and calculate score
     #  neighbors: The list of Node object
     def play_pd(self, neighbors):
-        """
-        timestep for node
-        """
         self.reset_score()
         for neighbor in neighbors:
             self.add_score(self.pd_score(neighbor.state))
 
     # play single PD
-    def pd_score(self, neighbor_state):
+    def pd_score(self, neighbor_state: str):
         if self.is_cooperator():
             if self.is_cooperator_state(neighbor_state):
                 return Node.R
@@ -47,10 +40,14 @@ class Node:
                 return Node.T
             return Node.P
 
+    # Copy the state of the most successful neighbor
     def get_max_state(self, neighbors):
         max_score = max([neighbor.score for neighbor in neighbors])
-        self.max_state = random.choice([neighbor.state for neighbor in neighbors if neighbor.score == max_score])
+        self.max_state = [neighbor.state for neighbor in neighbors if neighbor.score == max_score]
 
-    # Copy the state of the most successful neighbor
     def update_state(self):
-        self.state = self.max_state
+        self.state = self.max_state[0]
+
+    @classmethod
+    def change_rule(cls, T, R, P, S):
+        cls.T, cls.R, cls.P, cls.S = T, R, P, S
